3fcbd20c8341d6070458a06b57f6083051d81c79
NguyenPhong <phongnguyenphong267@gmail.com>
2024-11-10 00:46:48 +0700

    Remove bin and obj folders

---
+++ Garage/Forms/MainForm/Dictionary/DashboardControl.cs
@@ -0,0 +1,29 @@
+﻿using DocumentFormat.OpenXml.InkML;
+using Garage.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Garage.Forms.MainForm.Dictionary
+{
+    public partial class DashboardControl : UserControl
+    {
+        private FlowLayoutPanel statsPanel;
+     
+        private GaraOtoDbContext _contextOptions;
+
+        public DashboardControl(RevenueCalculator _revenueCalcualtor,GaraOtoDbContext context)
+        {
+         
+            InitializeComponent();
+        }
+
+      
+    }
+}
